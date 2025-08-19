d1b874de51bf3a617c82ef98b8c75aebd72fecf2
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2025-08-18 20:00:02 -0700

    update flake and change .dir-locals.el

--- flake.nix
+++ flake.nix
@@ -2,33 +2,30 @@
   description = "Nix template for Haskell projects";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-
-    nixos-unified.url = "github:srid/nixos-unified";
-    haskell-flake.url = "github:srid/haskell-flake";
-    fourmolu-nix.url = "github:jedimahdi/fourmolu-nix";
-
     agenix-shell = {
       url = "github:aciceri/agenix-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.flake = false;
   };
 
   outputs =
-    inputs:
-    # This will import ./nix/modules/flake/*.nix
-    # cf. https://nixos-unified.org/autowiring.html#flake-parts
-    #
-    # To write your own Nix, add or edit files in ./nix/modules/flake/
-    inputs.nixos-unified.lib.mkFlake {
-      inherit inputs;
-      root = ./.;
+    { nixpkgs, ... }:
+    let
+      game = nixpkgs.legacyPackages.x86_64-linux.callPackage ./. { };
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        inputsFrom = [ game.env ];
+        packages =
+          (with pkgs.haskellPackages; [
+            cabal-install
+            haskell-language-server
+            ghcid
+            hoogle
+            fourmolu
+          ])
+          ++ (with pkgs; [ just ]);
+      };
     };
 }
