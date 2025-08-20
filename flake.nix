6a0d34a91690818761480b0b865627e50c590a80
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2025-08-19 22:58:24 -0700

    add game to flake output

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
+      packages.x86_64-linux.default = game;
       devShells.x86_64-linux.default = pkgs.mkShell {
         inputsFrom = [ game.env ];
         packages =
